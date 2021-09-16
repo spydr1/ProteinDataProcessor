566eea460129019b2d2a3e1c2aa274b885c73626
MinjunJeon <ghkduadml@naver.com>
2021-09-16 06:50:17 +0000

    change the name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="pharmcadd-pdp",
+    name="pdp",
     version="0.0.1",
     author="Minjun Jeon, Byeongcheol JO, Yechan Hong",
     author_email="jmj@pharmcadd.com",
